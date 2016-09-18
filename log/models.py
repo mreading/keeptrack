5787bfaf18fe52bd45575a8b9a3bcfbfa3f29080
jackhpierce <jackhpierce@gmail.com>
2016-09-18 16:13:57 -0400

    database structure for workouts

--- log/models.py
+++ log/models.py
@@ -10,3 +10,54 @@ class Athlete(models.Model):
 
 class Coach(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Activity(models.Model):
+    athlete = models.ForeignKey(Athlete)
+    date = models.DateField()
+    #time
+    #weather
+    #gpx file
+
+class Race(models.Model):
+    activity = models.ForeignKey(Activity)
+    distance = models.FloatField()
+    time = models.DurationField()
+    location = models.CharField(max_length=100)
+    place = models.PositiveIntegerField()
+
+class NormalRun(models.Model):
+    activity = models.ForeignKey(Activity)
+    distance = models.FloatField()
+    time = models.DurationField
+    #shoe
+    #surface
+    #route
+
+class CrossTrain(models.Model):
+    activity = models.ForeignKey(Activity)
+    distance = models.FloatField()
+    time = models.DurationField()
+    sport = models.CharField(max_length = 20)
+
+
+class IntervalRun(models.Model):
+    activity = models.ForeignKey(Activity)
+    warmup = models.FloatField()
+    cooldown = models.FloatField()
+    total_distance = models.FloatField()
+
+class Rep(models.Model):
+    interval_run = models.ForeignKey(IntervalRun)
+    distance = models.FloatField()
+    time = models.DurationField()
+    goal_pace = models.FloatField()
+    position = models.PositiveIntegerField()
+
+class Thread(models.Model):
+    activity = models.ForeignKey(Activity)
+
+class Comment(models.Model):
+    thread = models.ForeignKey(Thread)
+    text = models.CharField(max_length=1500)
+    position = models.IntegerField()
+    private = models.BooleanField()
