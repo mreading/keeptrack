f92fa9ebd58b443a56772a1e066d88793f1fe5f8
Lexie Judd <lexiejudd@Lexies-MacBook-Air.local>
2016-09-19 13:48:27 -0400

    one more test

--- log/athlete_views.py
+++ log/athlete_views.py
@@ -7,5 +7,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+#change
 def athlete(request):
     return render(request, "log/athlete.html", {})
