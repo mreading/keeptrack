d3f885b9cac8612944523d2907be670ce692dc64
Lexie Judd <lexiejudd@Lexies-MacBook-Air.local>
2016-09-19 13:50:37 -0400

    undoing my last test comment

--- log/athlete_views.py
+++ log/athlete_views.py
@@ -7,6 +7,5 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-#change
 def athlete(request):
     return render(request, "log/athlete.html", {})
