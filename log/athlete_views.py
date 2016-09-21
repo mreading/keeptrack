d908ea76d04a02a10b5c4522922a1f1a43b3638c
jackhpierce <jackhpierce@gmail.com>
2016-09-20 20:05:35 -0400

    base.html clarification

--- log/athlete_views.py
+++ log/athlete_views.py
@@ -8,4 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 def athlete(request):
+    print Athlete.objects.get(user=request.user)
+    # print Coach.objects.get(user=request.user)
+    print "hello"
     return render(request, "log/athlete.html", {})
