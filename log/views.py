c0c334cd2713857a61791d404c5875827455376c
jhpierce <jackhpierce@gmail.com>
2016-09-28 13:49:17 -0400

    team context rendered in index view

--- log/views.py
+++ log/views.py
@@ -3,13 +3,14 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from .forms import *
 from .utils import *
+from .team_views import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 @login_required(login_url='/log/login/')
 def index(request):
-    return render(request, "log/team.html", {})
+    return team(request)
 
 def settings(request):
     return render(request, "log/settings.html", {})
