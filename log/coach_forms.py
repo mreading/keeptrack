f8a5ba3efeae56b5b69c9b32920aa7df079cd1a2
Lexie Judd <lexiejudd@lexies-air.hamilton.edu>
2016-10-05 12:20:04 -0400

    Create Team Form

--- log/coach_forms.py
+++ log/coach_forms.py
@@ -3,18 +3,26 @@ from django import forms
 from .models import *
 import datetime
 
+YEARS = [('2017', '2017'), ('2018', '2018'), ('2019', '2019'), ('2020', '2020')]
+YEAR_CHOICES = ('2017', '2018', '2019', '2020')
+GENDER_CHOICES = [('Men\'s', 'Mens'), ('Women\'s', 'Womens')]
+SPORT_CHOICES = [('ITF', 'ITF'), ('OTF','OTF'), ('XC', 'XC')]
 
 class NewTeamForm(forms.Form):
-    school = forms.CharField(max_length=50, label="School")
-    team = forms.CharField(max_length=50, label="Team")
-    season = forms.CharField(max_length=50, label="Season")
-    start_date = forms.DateField(initial=datetime.date.today, label = "Start Date")
-    end_date = forms.DateField(initial=datetime.date.today, label = "End Date")
-
-    """
-    team = forms.IntegerField(
+    school = forms.CharField(
         widget=forms.Select(
-            choices=Team.objects.all().values_list('id', 'school_name')
-            )
-         )
-"""
+            choices=Team.objects.all().values_list('id', 'school_name')), label = "School")
+    gender = forms.CharField(
+        widget=forms.Select(
+            choices=GENDER_CHOICES), label = "Gender")
+    sport = forms.CharField(
+        widget=forms.Select(
+            choices=SPORT_CHOICES), label = "Sport")
+    year = forms.IntegerField(
+            widget=forms.Select(choices=YEARS), label = "Year")
+
+    start_date = forms.DateField(
+        widget=forms.SelectDateWidget(years=YEAR_CHOICES), initial=datetime.date.today, label = "Start Date")
+
+    end_date = forms.DateField(
+        widget=forms.SelectDateWidget(years=YEAR_CHOICES), initial=datetime.date.today, label = "End Date")
--- log/coach_views.py
+++ log/coach_views.py
@@ -15,72 +15,30 @@ import json
 
 @login_required(login_url='/log/login/')
 def create_team(request, user_id):
-    # Make sure user is coach
+    #Make sure user is coach
     user = User.objects.get(id=user_id)
     athlete = user.athlete_set.all()
     coach = user.coach_set.all()
 
-    name = user.first_name
-
-    if athlete:
-        print("ATHLETE")
     if coach:
-        print("COACH")
-    else:
-        print("NEITHER")
-
-    # if user.athlete:
-    #     print("USER.ATHLETE")
-    # else:
-    #     print("NO USER.ATHLETE")
-    #
-    # athlete = Athlete.objects.get(user=user)
-    # #coach = Coach.objects.get(user=user)
-
-    # if coach:
-    #     print("COACH")
-    # elif athlete:
-    #     print("ATHLETE")
-    # else:
-    #     print("NEITHER COACH NOR ATHLETE")
-
-    if request.method == 'POST':
-        print("IN POST")
-        form = NewTeamForm(request.POST)
-        #if form.is_valid():
-        #    data = form.cleaned_data
-        if 0:
-
-
-            """user = User.objects.create_user(data['username'], data['email'], data['password'])
-            user.first_name = data['first_name']
-            user.last_name = data['last_name']
-            team = Team.objects.get(id=int(data['team']))
-
-            # Depending on whether the person signing up is a coach or an
-            # athlete, different objects need to be created.
-            if data['is_coach'] == True:
-                print "was coach"
-                coach = Coach.objects.create(user_id=user.id, team=team)
-                user.coach = coach
+        if request.method == 'POST':
+            form = NewTeamForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                team = Team.objects.create(school_name = data['school'], gender = data['gender'])
+                team.save()
+                season = Season.objects.create(team = team, year = data['year'], sport = data['sport'])
+                return redirect("/log", {})
             else:
-                print "was athlete"
-                athlete = Athlete.objects.create(
-                    user_id=user.id,
-                    graduation_year=data['graduation_year'],
-                    team = team
-                    #Probably other stuff here
-                    )
-                user.athlete = athlete
+                return render(request, "log/create_team.html", {'form':form})
 
-            user.save()
-            user = authenticate(username=data['username'], password=data['password'])
-            login(request, user)
-            """
-            return redirect("/log", {})
         else:
-            return render(request, "log/create_team.html", {'form':form, 'user_id':user_id})
+            form = NewTeamForm()
+            return render(request, "log/create_team.html", {'form':form})
+
+    elif athlete:
+        print "Athlete"
+        return render(request, "log/settings.html")
 
     else:
-        form = NewTeamForm()
-        return render(request, "log/create_team.html", {'form':form, 'user_id':user_id})
+        return render(request, "log/create_team.html")
