5269228eaf2ac3332646ec3e80639ed37ef494de
jackhpierce <jackhpierce@gmail.com>
2016-11-15 11:02:07 -0500

    add default location to athlete model, display finishing place activity_detail.html

--- log/models.py
+++ log/models.py
@@ -48,7 +48,7 @@ class Athlete(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     graduation_year = models.PositiveIntegerField()
     log_private = models.BooleanField(default=True)
-    default_location = models.CharField(max_length=50)
+    default_location = models.CharField(max_length=50, default="Kirkland, NY")
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
